b0a9de95c73d3aaf140eff07e543730ef618d1b9
Ubuntu <ubuntu@ip-172-31-18-128.eu-west-1.compute.internal>
2018-05-16 21:14:54 +0000

    Installed Sidekiq

--- app/models/diagnostic.rb
+++ app/models/diagnostic.rb
@@ -1,4 +1,12 @@
 class Diagnostic < ApplicationRecord
   belongs_to :user
   mount_uploader :photo, PhotoUploader
+  
+  after_save :async_update # Run on create & update
+  
+  private
+
+  def async_update
+    DiagnosticJob.perform_later(self.id)
+  end
 end
