25740810ccac5356f58f185a418ce2e6b6fefab5
Ubuntu <ubuntu@ip-172-31-18-128.eu-west-1.compute.internal>
2018-05-16 20:53:27 +0000

    removed access to tensorflow lib

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    url = 'http://ec2-34-240-3-230.eu-west-1.compute.amazonaws.com:5000/flossy?image=cavity_056.jpg'
-    @diagnostic = JSON.parse(open(url).read)
+    
   end
 end
